39140d6298803d46bb662ea225127d1639d8c642
hani <98776153+hzwain@users.noreply.github.com>
2023-11-25 23:24:18 +0300

    change color

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hani-guss</Text>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff1',
     alignItems: 'center',
     justifyContent: 'center',
   },
